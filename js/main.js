ad119f8c39e88bc5e7c92db599c5236ac78a0bbb
David Rangel <rangelography@gmail.com>
2017-01-17 14:00:39 -0600

    added boilerplate for portfolio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Grabbing react from node_modules
+import React from 'react'
+
+export default React.createClass({
+  render () {
+    return (
+      <section></section>
+    )
+  }
+})
---
+++ spec/testJsSpec.js
@@ -0,0 +1,6 @@
+// spec.js for the boilerplate
+describe("Is my jasmine working", function(){
+  it("should fail test", function(){
+    expect("not working?").toBe("working?")
+  })
+})
