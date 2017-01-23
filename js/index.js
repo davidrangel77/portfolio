e20f9334b3cfb9860b71f4ce1e57e4b57d8d85c0
David Rangel <rangelography@gmail.com>
2017-01-23 14:07:16 -0600

    integrated 8.1 lecture code into portfolio repo for heroku deployment

--- js/index.js
+++ js/index.js
@@ -7,11 +7,7 @@ import Resume from './resume'
 import Blog from './blog'
 import Projects from './projects'
 
-// https://github.com/ReactTraining/react-router
-// https://github.com/ReactTraining/react-router/blob/master/docs/API.md
-// https://developer.mozilla.org/en-US/docs/Web/API/History_API
-//  Push state allows us to have a meaningful browser history
-//    in order to use the browser's back button
+
 render(
   (
     <Router onUpdate={() => window.scrollTo(0, 0)} history={hashHistory}>
