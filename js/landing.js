ea914833eb43b926c1ede5cab50b3549feca0008
David Rangel <rangelography@gmail.com>
2017-01-24 10:25:51 -0600

    styled landing page and resume page

--- js/landing.js
+++ js/landing.js
@@ -9,9 +9,9 @@ export default React.createClass({
         <h1 className="pageHeadingTitle">Short Website About D Rangel</h1>
         <div className="content">
           <h2>Jr. Developer in training; San Antonio, Tx</h2>
-          <p className="landingPageText">Welcome!<br/>
-            My name is David Rangel and I am an aspiring front-end developer currently attending classes at <a href="https://www.theironyard.com/">The Ironyard</a> in San Antonio, Tx.<br/>
-          I have had many careers and experiences, but I believe I am on the path to the most successful time in my professional life.<br/>
+          <p className="landingPageText">Welcome!<br/><br/>
+            My name is David Rangel and I am an aspiring front-end developer currently attending classes at <a href="https://www.theironyard.com/">The Ironyard</a> in San Antonio, Tx.<br/><br/>
+          I have had many careers and experiences, but I believe I am on the path to the most successful time in my professional life.<br/><br/>
             Please use the links below to navigate through these pages. </p>
           <a href="https://github.com/davidrangel77/" target="_blank">see more work in progress on Github</a>
         </div>
