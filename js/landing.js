5327d2195d168458ea847f6153634ea441723266
David Rangel <rangelography@gmail.com>
2017-01-25 22:37:29 -0600

    added mobile device styling

--- js/landing.js
+++ js/landing.js
@@ -9,9 +9,10 @@ export default React.createClass({
         <h1 className="pageHeadingTitle">A Short Website About D Rangel</h1>
         <div className="landingWhole">
           <h2 className="landingTitle">Jr. Developer in training; San Antonio, Tx</h2>
-          <p className="landingPageText">Welcome!<br/><br/>
+          <p className="landingPageWelcome">Welcome!</p>
+          <p className="landingPageText">
             My name is David Rangel and I am an aspiring front-end developer currently attending the Front End Engineering course at <a href="https://www.theironyard.com/">The Ironyard</a> in San Antonio, Tx.<br/><br/>
-          I have had many careers and experiences, but I believe I am on the path to the most successful time in my professional life.<br/><br/>
+            I have had many careers and experiences, but I believe I am on the path to the most successful time in my professional life.<br/><br/>
         Please use the buttons below to navigate through these pages. </p>
         </div>
       </section>
